96e6f5f8801be7e616b7e7b6f6c1c9e12ec2ba6c
deserve82 <inth8204@gmail.com>
2022-11-28 20:52:24 +0900

    feat : cars 도메인 추가

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -1,13 +1,19 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Cars {
+
     private final List<Car> carList;
 
+    public Cars(List<Car> carList) {
+        this.carList = carList;
+    }
+
     public Cars() {
-        carList = new ArrayList<>();
+        this(new ArrayList<>());
     }
 
     public void addCar(Car car) {
@@ -16,5 +22,27 @@ public class Cars {
 
     public List<Car> getCarList() {
         return carList;
+    }
+
+    public List<Car> getHighestProgressCars() {
+        List<Car> cars = new ArrayList<>();
+        int pivot = 0;
+
+        for (Car car : carList) {
+            int progress = car.getProgress();
+            if (progress > pivot) {
+                cars = new ArrayList<>(Collections.singletonList(car));
+                pivot = progress;
+            } else if (progress == pivot) {
+                cars.add(car);
+            }
+        }
+
+        return cars;
+    }
+
+    public void increaseProgress(int index, int value) {
+        Car targetCar = carList.get(index);
+        targetCar.increaseProgress(value);
     }
 }
--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -3,10 +3,27 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 class CarsTest {
+
+    private Car a;
+    private Car b;
+    private Car c;
+    private Cars cars;
+
+    @BeforeEach
+    void init() {
+        a = new Car("a");
+        b = new Car("b");
+        c = new Car("c");
+        cars = new Cars(Arrays.asList(a, b, c));
+    }
 
     @Test
     void test_create_cars() {
@@ -20,5 +37,48 @@ class CarsTest {
 
         cars.addCar(car);
         assertThat(cars.getCarList().get(0)).isEqualTo(car);
+    }
+
+    @Test
+    void test_get_highest_progress_test() {
+        a.increaseProgress(5);
+
+        List<Car> highestCars = cars.getHighestProgressCars();
+
+        assertThat(highestCars.size()).isEqualTo(1);
+        assertThat(highestCars.get(0)).isEqualTo(a);
+    }
+
+    @Test
+    void test_get_highest_progress_test_with_more_than_two() {
+        a.increaseProgress(5);
+        b.increaseProgress(4);
+
+        List<Car> highestCars = cars.getHighestProgressCars();
+
+        assertThat(highestCars.size()).isEqualTo(2);
+    }
+
+    @Test
+    void test_get_highest_progress_test_with_more_than_two_2() {
+        b.increaseProgress(4);
+        b.increaseProgress(7);
+        c.increaseProgress(4);
+
+        List<Car> highestCars = cars.getHighestProgressCars();
+
+        assertThat(highestCars.size()).isEqualTo(1);
+        assertThat(highestCars.get(0)).isEqualTo(b);
+    }
+
+    @Test
+    void test_increase_progress_with_index() {
+        cars.increaseProgress(0, 8);
+        cars.increaseProgress(1, 2);
+        cars.increaseProgress(1, 1);
+
+        assertThat(cars.getCarList().get(0).getProgress()).isEqualTo(1);
+        assertThat(cars.getCarList().get(1).getProgress()).isEqualTo(0);
+        assertThat(cars.getCarList().get(2).getProgress()).isEqualTo(0);
     }
 }
